268daec9d6345073cf01f9451b89a866e0941901
BenGurWaves <bengurwaves@gmail.com>
2025-11-22 07:08:36 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 export default nextConfig
